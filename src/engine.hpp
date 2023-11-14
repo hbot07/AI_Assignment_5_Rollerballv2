cf9a1434b40e53f3addcfb22685b139f78d87e4b
DonKans <97870053+DonKans@users.noreply.github.com>
2023-11-14 22:27:29 +0530

    Update engine.hpp

--- src/engine.hpp
+++ src/engine.hpp
@@ -8,7 +8,9 @@ class Engine : public AbstractEngine {
     // add extra items here. 
     // Note that your engine will always be instantiated with the default 
     // constructor.
-    
+    int num_moves = 0;
+    float move_time = 2500;
+    int minimax(const Board &b, int depth, bool isMaximizingPlayer, int alpha, int beta,auto start_time);
     public:
     void find_best_move(const Board& b) override;
 
